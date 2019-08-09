6e9a88e2027b7df73fba17622f3482a4c16f1714
Luis Ribeiro <luis.ribeiro@stud.fh-campuswien.ac.at>
2019-08-09 21:28:55 +0200

    CASP-189
    Register is running again

--- src/main/java/casp/web/app/ServiceRegistryApplication.java
+++ src/main/java/casp/web/app/ServiceRegistryApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaServer        // Enable eureka server
-@EnableHystrix
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
