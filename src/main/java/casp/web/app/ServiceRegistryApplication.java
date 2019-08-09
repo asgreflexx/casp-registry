c5c9f774222f099f400a81bda3bf3b18d5e76878
Luis <lacribeiro11@gmail.com>
2019-08-09 14:08:31 +0200

    CASP-189
    create a new microservice

--- src/main/java/casp/web/app/ServiceRegistryApplication.java
+++ src/main/java/casp/web/app/ServiceRegistryApplication.java
@@ -3,9 +3,11 @@ package casp.web.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaServer        // Enable eureka server
+@EnableHystrix
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
