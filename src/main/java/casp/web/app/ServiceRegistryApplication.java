80088b5df6898cbef9e9fc9f18bc5c92c8df14ba
Luis <luis.ribeiro@stud.fh-campuswien.ac.at>
2020-02-24 22:34:01 +0100

    LRBAC-3 Die version von registry server läuft schon

--- src/main/java/casp/web/app/ServiceRegistryApplication.java
+++ src/main/java/casp/web/app/ServiceRegistryApplication.java
@@ -1,13 +1,11 @@
 package casp.web.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableEurekaServer        // Enable eureka server
+@EnableEurekaServer      // Enable eureka server
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
