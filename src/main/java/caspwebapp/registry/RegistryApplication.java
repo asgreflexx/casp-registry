3a0a96e9c14c3e0f1a77de5f1fbe88544da6b6b1
Luis Ribeiro <luis.ribeiro@stud.fh-campuswien.ac.at>
2020-10-21 23:30:30 +0200

    CASP-35 Set the project as a eureka server discovery service

--- src/main/java/caspwebapp/registry/RegistryApplication.java
+++ src/main/java/caspwebapp/registry/RegistryApplication.java
@@ -1,15 +1,15 @@
-package casp.web.app;
+package caspwebapp.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-@EnableEurekaServer      // Enable eureka server
-public class ServiceRegistryApplication {
+public class RegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistryApplication.class, args);
+        SpringApplication.run(RegistryApplication.class, args);
     }
 
 }
